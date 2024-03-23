c71cf562be986629368dd447a99be5dc7ccdae9d
dbrowner1 <dbrowner1@ggc.edu>
2024-03-22 22:19:15 -0400

    init commit

--- src/FarmAnimals/Chicken.java
+++ src/FarmAnimals/Chicken.java
@@ -1,3 +1,9 @@
+/**Class: Chicken
+ * @author D Browner
+ * @version 1.6
+ * Course: ITEC 2140 Spring 2023
+ * Written: March 12, 2024
+ */
 package FarmAnimals;
 
 public class Chicken extends FarmAnimal {
